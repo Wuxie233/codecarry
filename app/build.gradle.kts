97acb6029c4eb8e7d7ac0b81371bdf62bef60a2a
Wuxie233 <445714414@qq.com>
2026-09-08 20:00:52 +0800

    chore(release): prepare CodeCarry 1.14.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "dev.wuxie233.codecarry"
         minSdk = 26
         targetSdk = 34
-        versionCode = 123
-        versionName = "1.13.3"
+        versionCode = 124
+        versionName = "1.14.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
